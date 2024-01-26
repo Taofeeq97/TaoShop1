ac279b955589c871227855ad09fe30d06e7402f3
Taofeeq97 <otutaofeeqi@gmail.com>
2024-01-26 23:19:54 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {
+    Container,
+    Nav, 
+    Navbar, 
+    NavDropdown
+} from 'react-bootstrap';
+
+
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" expand="lg" className="bg-body-tertiary" collapseOnSelect>
+      <Container>
+        <Navbar.Brand href="/">TaoShop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+            <Nav.Link href="/"><i className='fas fa-user'></i>Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </header>
+  )
+}
+
+export default Header
